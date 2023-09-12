ff98969b8714da4ff003c0025f4856e32160adb9
SuZeAI <goohaa1234@gmail.com>
2023-09-12 20:45:37 +0700

    PROGRAMPYTHON

--- sothuannghichchan.py
+++ sothuannghichchan.py
@@ -1,20 +1,18 @@
-a = []
-b = ['0', '2', '4', '6', '8']
-def Try(s) :
-    k = list(s)
-    k.reverse()
-    k = int(s + ''.join(k))
-    global a
-    a = a + [k]
-    if(len(s) != 3) :
-        for i in b :
-            Try(s + i)
-for i in range(1, 5) : Try(b[i])
-a.sort()
 t = int(input())
-for i in range(t) :
+
+for _ in range(t):
     n = int(input())
-    for j in a :
-        if j < n : print(j, end = " ")
-        else : break
+    ls = ["2", "4", "6", "8"]
+    while True:
+        front = ls.pop(0)
+        if int(front + front[::-1]) >= n:
+            break
+        else:
+            print(front + front[::-1], end=" ") 
+        ls.append(front + "0")
+        ls.append(front + "2")
+        ls.append(front + "4")
+        ls.append(front + "6")
+        ls.append(front + "8")
+        
     print()
